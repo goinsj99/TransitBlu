969730bd8cb3b279b74575660acee5c33683846a
Jon Goins <82783205+goinsj99@users.noreply.github.com>
2024-10-01 15:39:23 -0500

    testingcodechunks/website

--- login.js
+++ login.js
@@ -1,99 +1,108 @@
-//Javascript for the login functionality
-"use strict";
-let current_username;
-(function() {
-
-  window.addEventListener("load", init);
-
-  //Adds an event listener to the add user form, pointing to the api call function.
-  async function init() {
-    let addForm = document.getElementById("add-form");
-    addForm.addEventListener("submit", async function (event) {
-      let res = await addUser(event);
-      if(typeof(res) == 'string'){
-        window.alert('Error');
+function openModal() {
+    /* Note that you do NOT have to do a document.getElementById anywhere in this exercise. Use the elements below */
+    var myInput = document.getElementById("psw");
+    var confirmMyInput = document.getElementById("cpsw");
+    var letter = document.getElementById("letter");
+    var capital = document.getElementById("capital");
+    var number = document.getElementById("number");
+    var length = document.getElementById("length");
+    var match = document.getElementById("match");
+  
+    // When the user starts to type something inside the password field
+    myInput.onkeyup = function () {
+      console.log("helllooo");
+  
+      /* TODO: Question 1.1: Starts here */
+      var lowerCaseLetters = /[a-z]/g; // : Fill in the regular experssion for lowerCaseLetters
+      var upperCaseLetters = /[A-Z]/g; // : Fill in the regular experssion for upperCaseLetters
+      var numbers = /[0-9]/g; // : Fill in the regular experssion for digits
+      var minLength = 8; // : Change the minimum length to what what it needs to be in the question
+      /* TODO: Question 1.1: Ends here */
+  
+      /* TODO: Question 1.2:  Starts here */
+      /*
+           - So first read up on classList.  
+           - Perform a console.log(letter.classList) and check the array that you see. By default the first time, there should be just 1 element and it should be
+           "invalid". "invalid" is a class that is present in login.css. 
+           - Below, there are a bunch of if blocks and else blocks.
+           - Each if block means that some successful condition is satisfied for our password conditon. So the red cross need to be converted to a check mark.
+           - Each else block stands for a failed condition, so the green check mark needs to be a red cross again.
+           - All that you need to do is, in each of the blocks, fill in the correct classNames for the remove and the add methods.
+           */
+      console.log(letter.classList)
+      // Validate lowercase letters
+      if (myInput.value.match(lowerCaseLetters)) {
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
       } else {
-        window.alert('Successfully Registered');
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
       }
-    });
-
-    //Adds an event listener to the login form, pointing to the api call function.
-    let loginForm = document.getElementById("login-form");
-    loginForm.addEventListener("submit", async function (event) {
-      let res = await login(event);
-      if (typeof(res) == 'string') {
-        window.alert('Incorrect Username or Password');
+  
+      // Validate capital letters
+      if (myInput.value.match(upperCaseLetters)) {
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
       } else {
-        window.alert('Successfully logged in');
-        document.getElementById("homeLink").click();
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
       }
-      window.sessionStorage.setItem('username', res.username);
-    });
+  
+      // Validate numbers
+      if (myInput.value.match(numbers)) {
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+      } else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+      }
+  
+      // Validate length
+      if (myInput.value.length >= minLength) {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+      } else {
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+      }
+      /* TODO: Question 1.2:  Ends here */
+    };
+    /* TODO Question 1.3: Starts here */
+    confirmMyInput.onkeyup = function () {
+      // Validate password and confirmPassword
+      var passEqualsConfPass = (myInput.value == confirmMyInput.value); // TODO: Change this to the condition that needs to be checked so that the text entered in password equals the text in confirm password
+      if (passEqualsConfPass) {
+        match.classList.remove("invalid");
+        match.classList.add("valid");
+      } else {
+        match.classList.remove("valid");
+        match.classList.add("invalid");
+      }
+      /* TODO Question 1.3: Starts here */
+  
+      // Disable or Enable the button based on the elements in classList
+      enableButton(letter, capital, number, length, match);
+    };
   }
-
-  /**
-   * Makes a post request to the bus API, adding a new user from the formdata.
-   * Username and password are necessary fields for the post request.
-   * @param {Event} event Form submit event, used for event.target (the form)
-   * @returns {String} Success message from API
-   */
-  async function addUser(event) {
-    //Prevents the page from clearing when the form is submitted
-    event.preventDefault();
-    try {
-      //converts the filled out form into a form data object
-      let form = new FormData(event.target);
-      //creates a post request to the API with the formData as the body
-      let res = await fetch("/newUser", {
-        method: "POST",
-        body: form
-      });
-      //ensures the response from the API is ok, throws error if not.
-      res = await statusCheck(res);
-      //converts the json response to an object
-      res = await res.json();
-      //returns the object
-      return res.response;
-    } catch (err) {
-      return "Unsuccessful fetch request";
+  
+  function enableButton(letter, capital, number, length, match) {
+    // TODO: Clear this function for students to implement
+    var button = document.getElementById("my_submit_button");
+    var anchor = document.createElement('a');
+    anchor.href = "../../../lab-4-html-css-Oleracea/Personal_Website/mypage.html";
+    anchor.append(button)
+    var condition = (capital.classList == "valid" && letter.classList == "valid" && number.classList == "valid" && length.classList =="valid" && match.classList == "valid"); // TODO: Replace false with the correct condition
+    if (condition) {
+      button.disabled = false;
+      button.onclick = "onClickFunction()";
     }
   }
-
-  /**
-   * Makes a post request to the bus API, logging in a user
-   * Username and password are necessary fields for the post request.
-   * @param {Event} event Form submit event, used for event.target (the form)
-   * @returns response object containing the users info.
-   */
-  async function login(event) {
-    //Prevents the page from clearing when the form is submitted
-    event.preventDefault();
-    try {
-      let form = new FormData(event.target);
-      let res = await fetch("/login", {
-        method: "POST",
-        body: form
-      });
-      //ensures the response from the API is ok, throws error if not.
-      res = await statusCheck(res);
-      //converts the json response to an object
-      res = await res.json();
-      //returns the object
-      return res.response;
-    } catch (err) {
-      return "Unsuccessful fetch request";
-    }
+  
+  function onClickFunction() {
+    alert("Hey! I'm all green! Well done.");
   }
+  
 
-  async function statusCheck(response) {
-    if (!response.ok) {
-      throw new Error(await response.text());
-    }
-    return response;
+  function problemReported() {
+    alert("Thank you for your feedback, we will be using this for absolutely nothing");
   }
-
-})();
-
-function logOut(){
-  window.sessionStorage.clear();
-}
--- mapsAPICall.js
+++ mapsAPICall.js
@@ -1,4 +1,17 @@
-//File for interacting with google maps API and displaying map in page.
+
+
+
+function addRoute()
+{
+  document.getElementById("fromloc").value;
+  document.getElementById("fromloc").value;
+  document.getElementById("TotalTime").innerHTML;
+  document.getElementById("TotalDistance").innerHTML;
+//write to data base here dont Know if it really makes sense to store more 
+//than the locations becouse we will have to find the time again and to have
+//the on time boolean we woul dhave to change the way the whole the database works
+
+}
 let map;
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
@@ -16,27 +29,15 @@ function initMap() {
           //var marker = new google.maps.Marker({position: pos,map:map});
         });
     }
-    const options = {
-      fields: ["formatted_address", "geometry", "name"],
-      strictBounds: false,}
-      const autocompleteFrom = new google.maps.places.Autocomplete(document.getElementById("fromloc"), options);
-      const autocompleteTo = new google.maps.places.Autocomplete(document.getElementById("toloc"), options);
 }
 
 function makeRoute(){
     let directionsRequest = new google.maps.DirectionsService();
     let directionsDisplay = new google.maps.DirectionsRenderer();
-    initMap();
     directionsDisplay.setMap(map);
-    if(document.getElementById("mapPanel").innerHTML!=""){
-      document.getElementById("mapPanel").innerHTML="";
-    }
-    document.getElementById("mapPanel").style.display = 'none';
-    document.getElementById("showPanel").style.display = 'block';
-    directionsDisplay.setPanel(document.getElementById("mapPanel"));
     if(document.getElementById("fromloc").value == "")
     {
-      if (navigator.geolocation)
+      if (navigator.geolocation) 
       {
         var options = {
           enableHighAccuracy: true,
@@ -70,7 +71,7 @@ function makeRoute(){
                     {
                       walk_info[2] = document.getElementById("walkingSpeed").value;
                     }
-
+                    
                     for(var i=0; i < result.routes[0].legs[0].steps.length;i++)
                     {
                       if(result.routes[0].legs[0].steps[i].travel_mode == google.maps.TravelMode.WALKING)
@@ -81,23 +82,29 @@ function makeRoute(){
                       }
                       else
                       {
-
+                        
                         bus_info[0] += result.routes[0].legs[0].steps[i].duration.value;
                         bus_info[1] += result.routes[0].legs[0].steps[i].distance.value;
                       }
 
                       var time = bus_info[0] + walk_info[0];
-                      var distance = (bus_info[1] + walk_info[1])/1000;
+                      var distance = (bus_info[1] + walk_info[1])/1609;
                     }
-                     //display route
+                    document.getElementById("TotalTime").innerHTML = time + " seconds";
+                    document.getElementById("BusTime").innerHTML = bus_info[0] + " seconds";
+                    document.getElementById("WalkingTime").innerHTML= walk_info[0] + " seconds";
+                    document.getElementById("TotalDistance").innerHTML = distance + " miles";
+                    document.getElementById("BusDistance").innerHTML = bus_info[1] + " meters";
+                    document.getElementById("WalkingDistance").innerHTML = walk_info[1] + " meters";
+                    //display route
                     directionsDisplay.setDirections(result);
                 } else {
                     //delete route from map
                     directionsDisplay.setDirections({ routes: [] });}
             });
-
+            
             //var marker = new google.maps.Marker({position: pos,map:map});
-
+          
           },error,options);
       }
     }
@@ -119,7 +126,7 @@ function makeRoute(){
           {
             walk_info[2] = document.getElementById("walkingSpeed").value;
           }
-
+          
           for(var i=0; i < result.routes[0].legs[0].steps.length;i++)
           {
             if(result.routes[0].legs[0].steps[i].travel_mode == google.maps.TravelMode.WALKING)
@@ -130,7 +137,7 @@ function makeRoute(){
             }
             else
             {
-
+              
               bus_info[0] += result.routes[0].legs[0].steps[i].duration.value;
               bus_info[1] += result.routes[0].legs[0].steps[i].distance.value;
             }
@@ -138,98 +145,38 @@ function makeRoute(){
             var time = bus_info[0] + walk_info[0];
             var distance = (bus_info[1] + walk_info[1])/1609;
           }
+          document.getElementById("TotalTime").innerHTML = time + " seconds";
+          document.getElementById("BusTime").innerHTML = bus_info[0] + " seconds";
+          document.getElementById("WalkingTime").innerHTML= walk_info[0] + " seconds";
+          document.getElementById("TotalDistance").innerHTML = distance + " miles";
+          document.getElementById("BusDistance").innerHTML = bus_info[1] + " miles";
+          document.getElementById("WalkingDistance").innerHTML = walk_info[1] + " miles";
           //display route
           directionsDisplay.setDirections(result);
         } else {
             //delete route from map
             directionsDisplay.setDirections({ routes: [] });}
-    });
+    }); 
 }
 
-function makeFavRoute(origin, destination){
-  console.log('making route');
-  document.getElementById("fromloc").value = origin;
-  document.getElementById("toloc").value = destination;
-  initMap();
+function makeFavRoute(){
   let directionsRequest = new google.maps.DirectionsService();
   let directionsDisplay = new google.maps.DirectionsRenderer();
   directionsDisplay.setMap(map);
-  if(document.getElementById("mapPanel").innerHTML!=""){
-    document.getElementById("mapPanel").innerHTML="";
-  }
-  document.getElementById("mapPanel").style.display = 'none';
-  document.getElementById("showPanel").style.display = 'block';
-  directionsDisplay.setPanel(document.getElementById("mapPanel"));
-  if(origin == "")
-  {
-    if (navigator.geolocation)
-    {
-      var options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-      };
-      function error()
-      {
-        console.log("did not work")
-      }
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          const request =
-          {
-            origin: pos,
-            destination: destination,
-            travelMode: google.maps.TravelMode.TRANSIT,
-            unitSystem: google.maps.UnitSystem.IMPERIAL
-          }
-          directionsRequest.route(request, function (result, status) {
-            if (status == google.maps.DirectionsStatus.OK) {
-              var bus_info = [0,0];
-              //time folowed by distance
-              var walk_info = [0,0,5];
-              //time,distance,speed
-              if(document.getElementById("walkingSpeed").value.length > 0)
-              {
-                walk_info[2] = document.getElementById("walkingSpeed").value;
-              }
-
-              for(var i=0; i < result.routes[0].legs[0].steps.length;i++)
-              {
-                if(result.routes[0].legs[0].steps[i].travel_mode == google.maps.TravelMode.WALKING)
-                {
-
-                    walk_info[0] += (result.routes[0].legs[0].steps[i].duration.value)*(5/(walk_info[2]));
-                    walk_info[1] += result.routes[0].legs[0].steps[i].distance.value;
-                }
-                else
-                {
-
-                  bus_info[0] += result.routes[0].legs[0].steps[i].duration.value;
-                  bus_info[1] += result.routes[0].legs[0].steps[i].distance.value;
-                }
-
-                var time = bus_info[0] + walk_info[0];
-                var distance = (bus_info[1] + walk_info[1]);
-              }
-              //display route
-              directionsDisplay.setDirections(result);
-            } else {
-                //delete route from map
-                directionsDisplay.setDirections({ routes: [] });}
-              });
-            },error,options);
-        }
-      }
-    else
-  {
   const request ={
-    //Pull from data base here
-      origin: origin,
-      destination: destination,
+    //Pull from data base here 
+
+
+
+
+
+
+
+
+
+
+      origin:"600 30th St, Boulder, CO 80303",
+      destination:"Economics Building, 261 UCB #212, Boulder, CO 80302",
       travelMode: google.maps.TravelMode.TRANSIT,
       unitSystem: google.maps.UnitSystem.IMPERIAL
   }
@@ -243,7 +190,7 @@ function makeFavRoute(origin, destination){
         {
           walk_info[2] = document.getElementById("walkingSpeed").value;
         }
-
+        
         for(var i=0; i < result.routes[0].legs[0].steps.length;i++)
         {
           if(result.routes[0].legs[0].steps[i].travel_mode == google.maps.TravelMode.WALKING)
@@ -254,69 +201,39 @@ function makeFavRoute(origin, destination){
           }
           else
           {
+            
             bus_info[0] += result.routes[0].legs[0].steps[i].duration.value;
             bus_info[1] += result.routes[0].legs[0].steps[i].distance.value;
           }
+
           var time = bus_info[0] + walk_info[0];
-          var distance = (bus_info[1] + walk_info[1]);
+          var distance = (bus_info[1] + walk_info[1])/1609;
+          
         }
+        document.getElementById("TotalTime").innerHTML = secondsToDhms(time);
+        document.getElementById("BusTime").innerHTML = secondsToDhms(bus_info[0]);
+        document.getElementById("WalkingTime").innerHTML=secondsToDhms(walk_info[0]);
+        document.getElementById("TotalDistance").innerHTML = distance + " meters";
+        document.getElementById("BusDistance").innerHTML = bus_info[1] + " meters";
+        document.getElementById("WalkingDistance").innerHTML = walk_info[1] + " meters";
         //display route
         directionsDisplay.setDirections(result);
       } else {
           //delete route from map
           directionsDisplay.setDirections({ routes: [] });}
-      });
-    }
-  }
+  });
+}
+
 function secondsToDhms(seconds) {
   seconds = Number(seconds);
   var d = Math.floor(seconds / (3600*24));
   var h = Math.floor(seconds % (3600*24) / 3600);
   var m = Math.floor(seconds % 3600 / 60);
   var s = Math.floor(seconds % 60);
-
-  var dDisplay = d > 0 ? d + (d == 1 ? " day " : " days ") : "";
-  var hDisplay = h > 0 ? h + (h == 1 ? " hour " : " hours ") : "";
-  var mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
+  
+  var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+  var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+  var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
   var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
   return dDisplay + hDisplay + mDisplay + sDisplay;
-  }
-
-  function getTime(time){
-    let matches = time.match(/\d+/g);
-    let day = time.match("day" || "days");
-    let number = 0;
-    if(matches.length==1){
-      number = parseInt(matches[0]);
-    }
-    console.log("days: ", day);
-    if(matches.length==2){
-      if(!day){
-      number = (parseInt(matches[0])*60)+parseInt(matches[1]);
-      }
-      else{
-        number = (parseInt(matches[0])*1440)+parseInt(matches[1]*60);
-      }
-    }
-    return number*60;
-  }
-
-function showDirections(){
-  let jst = document.querySelectorAll('[jstcache="82"]');
-  let modifier = 1.0;
-  let timeRemoved = 0;
-  let timeMod = 0;
-  let time = 0;
-  if(document.getElementById("walkingSpeed").value>0){
-    modifier = (3/document.getElementById("walkingSpeed").value);
-  }
-  for(let i = 1;i<jst.length;i++){
-    time=getTime(jst[i].innerHTML);
-    timeMod=Math.ceil(time*modifier);
-    jst[i].innerHTML=secondsToDhms(timeMod);
-    timeRemoved+=time-timeMod;
-  }
-  jst[0].innerHTML=secondsToDhms(getTime(jst[0].innerHTML)-timeRemoved);
-  document.getElementById("mapPanel").style.display = 'block';
-  document.getElementById("showPanel").style.display = 'none';
   }
